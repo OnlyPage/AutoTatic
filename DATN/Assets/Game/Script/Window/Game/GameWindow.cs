57115f16618f7295c75ce63dbc1e71d323352b34
User <lenhattan17tclc1@gmail.com>
2021-11-20 16:32:46 +0700

    Edit button login logout

---
+++ DATN/Assets/Game/Script/Window/Game/GameWindow.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GameWindow : MonoBehaviour
+{
+    [SerializeField]
+    private TMPro.TextMeshProUGUI userName;
+    // Start is called before the first frame update
+    void Start()
+    {
+        userName.text = "Welcome  " + PlayerPrefs.GetString(DataPlayerprefer.USERNAME);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void OnClick_Logout()
+    {
+        SceneManager.LoadScene(Scenes.LOGIN_SCENE, LoadSceneMode.Single);
+    }    
+}
